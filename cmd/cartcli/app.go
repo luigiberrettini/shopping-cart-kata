8a20321dbc366b1a94c98c3edc50a6d4937fdaf5
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-06-23 23:21:30 +0100

    Simplify requests

--- cmd/cartcli/app.go
+++ cmd/cartcli/app.go
@@ -34,8 +34,8 @@ func (a *App) createCart() (cart, int, string, error) {
 	if err != nil {
 		return c, 0, "", ErrReqPreparation
 	}
-	i, code, msg, err := performReq(a, req, &c)
-	return *i.(*cart), code, msg, err
+	code, msg, err := performReq(a, req, &c)
+	return c, code, msg, err
 }
 
 func (a *App) addArticleToCart(id string, etag string, aCod string, aQty int) (article, int, string, error) {
@@ -53,8 +53,8 @@ func (a *App) addArticleToCart(id string, etag string, aCod string, aQty int) (a
 	if etag != "" {
 		req.Header.Set("If-Match", etag)
 	}
-	i, code, msg, err := performReq(a, req, &art)
-	return *i.(*article), code, msg, err
+	code, msg, err := performReq(a, req, &art)
+	return art, code, msg, err
 }
 
 func (a *App) getCart(id string, etag string) (cart, int, string, error) {
@@ -67,8 +67,8 @@ func (a *App) getCart(id string, etag string) (cart, int, string, error) {
 	if etag != "" {
 		req.Header.Set("If-None-Match", etag)
 	}
-	i, code, msg, err := performReq(a, req, &c)
-	return *i.(*cart), code, msg, err
+	code, msg, err := performReq(a, req, &c)
+	return c, code, msg, err
 }
 
 func (a *App) deleteCart(id string, etag string) (int, string, error) {
@@ -80,24 +80,22 @@ func (a *App) deleteCart(id string, etag string) (int, string, error) {
 	if etag != "" {
 		req.Header.Set("If-Match", etag)
 	}
-
-	_, code, msg, err := performReq(a, req, nil)
-	return code, msg, err
+	return performReq(a, req, nil)
 }
 
-func performReq(a *App, req *http.Request, i interface{}) (interface{}, int, string, error) {
+func performReq(a *App, req *http.Request, i interface{}) (int, string, error) {
 	resp, err := a.HTTPClient.Do(req)
 	if err != nil {
-		return nil, resp.StatusCode, "", ErrReqExecution
+		return resp.StatusCode, "", ErrReqExecution
 	}
 
 	defer resp.Body.Close()
 	rb, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, resp.StatusCode, "", ErrRespRead
+		return resp.StatusCode, "", ErrRespRead
 	}
 	if len(rb) == 0 {
-		return nil, resp.StatusCode, "", nil
+		return resp.StatusCode, "", nil
 	}
 	var e apiError
 	err1 := json.NewDecoder(bytes.NewReader(rb)).Decode(&e)
@@ -106,11 +104,11 @@ func performReq(a *App, req *http.Request, i interface{}) (interface{}, int, str
 		err2 = json.NewDecoder(bytes.NewReader(rb)).Decode(&i)
 	}
 	if err1 != nil && err2 != nil {
-		return nil, resp.StatusCode, "", ErrRespDecode
+		return resp.StatusCode, "", ErrRespDecode
 	}
 	if etag := resp.Header.Get("ETag"); err2 == nil && len(etag) != 0 {
 		c := i.(*cart)
 		c.ETag = etag
 	}
-	return i, resp.StatusCode, e.Msg, nil
+	return resp.StatusCode, e.Msg, nil
 }
